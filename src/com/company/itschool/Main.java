9eff12f09096fc9c2592daec32eebec44b6bdb88
Gavrilovici <igabriel1993@gmail.com>
2021-02-15 20:31:12 +0200

    Problema cu blocul

--- src/com/company/itschool/Main.java
+++ src/com/company/itschool/Main.java
@@ -1,8 +1,24 @@
 package com.company.itschool;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        Bloc blocA1 =  new Bloc(12,"Scara 1");
+        System.out.println("Care este numele blocului?");
+        String numeBloc =  scanner.nextLine(); // E2
+        blocA1.setNumeBloc(numeBloc);
+        String adresa = blocA1.getAdresa();
+        System.out.println(adresa);
+
+        System.out.println("Introduce-ti pretul pe etaj:");
+        int pret =  scanner.nextInt();
+        scanner.nextLine();
+        blocA1.setPretPeEtaj(pret);
+
+        int total =  blocA1.calculateTotalPrice();
+        System.out.println("Te costa : " + total);
     }
 }
